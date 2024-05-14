6a79379e6f00a59acfbaa8b7d22e9dc2e298f5b5
revathi <arevathi125@gmail.com>
2024-05-13 20:49:23 -0500

    sign up pt2

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("add")
-    private String displayAddUserForm(){
+    private String displayAddUserForm(Model model){
+       model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping("")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String confirmPassword){
-        if (user.getPassword().equals(confirmPassword)){
-            model.addAttribute("user", user);
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors){
+       // if (user.getPassword().equals(confirmPassword)){
+        if (!errors.hasErrors()){
+           // model.addAttribute("user", user);
               return "user/index.html";
         }
         else {
-            model.addAttribute("error", "Passwords does not match");
+           // model.addAttribute("error", "Passwords does not match");
            return "user/add.html";
         }
     }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,20 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 5 , max = 15, message = "User name must be 5-15 chars long.")
     private String username;
+    @Email(message = "Invalid email. Try again. ")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "The Password should have at least 6 chars long.")
     private String password;
+
+    @Size(min = 6, message = "Confirm Password must be at least 6 characters long.")
+    @NotNull(message = "Confirm Passwords do not match.")
+    private String confirmPassword;
 
     public User(){
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String confirmPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.confirmPassword = confirmPassword;
     }
 
     public String getUsername() {
@@ -41,5 +53,16 @@ public class User {
         this.password = password;
     }
 
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
 
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    private void checkPassword(){
+        if (password != null && confirmPassword != null && !password.equals(confirmPassword))
+            setConfirmPassword(null);
+    }
 }
